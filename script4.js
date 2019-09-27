88826da23cf00cd4f6333bebf23b776e56603376
NIkitaKrupnov <55488626+NIkitaKrupnov@users.noreply.github.com>
2019-09-27 10:09:57 +0300

    Add files via upload

--- script4.js
+++ script4.js
@@ -1,25 +1,51 @@
 let a=9;
 let b=3;
-add(a,b);
+let j=prompt("какое действие выполнить?")
+if (j==="+") {
+  add(a,b)
+} else if (j==="-") {
+  abc(a,b)
+
+} else if (j==="/") {
+  abv(a,b)
+} else if (j==="*") {
+  abe(a,b)
+} else {
+alert("младший лейтенант, мальчик молодой")
+}{
+
+} {
+
+}{
+
+}
+
+
+
+
+
+
+
+
 function add(num1,num2) {
   let c=num1+num2
   alert(c)
 
 }
 
-abc(a,b)
+
 function abc(num1,num2) {
   let c=num1-num2
   alert(c)
 }
 
-abv(a,b)
+
 function abv(num1,num2) {
   let c=num1/num2
   alert(c)
 }
 
-abe(a,b)
+
 function abe(num1,num2) {
   let c=num1*num2
   alert(c)
